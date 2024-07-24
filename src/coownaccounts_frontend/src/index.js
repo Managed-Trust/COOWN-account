bb1337f6748da94b51f62e8b21266c29f0fbe9cf
Abdullah-1057 <abdullah.hkt@gmail.com>
2024-07-24 13:25:54 +0500

    Initial commit.

---
+++ src/coownaccounts_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { coownaccounts_backend } from "../../declarations/coownaccounts_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await coownaccounts_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
